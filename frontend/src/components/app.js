ec0ae49978f341b0d7ed7f449c6f100d5fd16450
Onyi Cho <onyicho@gmail.com>
2019-07-14 21:54:09 -0700

    Add Scoreboard to frontend route for debugging

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -6,6 +6,7 @@ import NavBarContainer from './nav/navbar_container';
 import MainPage from './main/main_page';
 import LoginFormContainer from './session/login_form_container';
 import SignupFormContainer from './session/signup_form_container';
+import ScoreboardContainer from './scoreboard/scoreboard_container';
 
 const App = () => (
   <div>
@@ -14,6 +15,7 @@ const App = () => (
       <AuthRoute exact path="/" component={MainPage} />
       <AuthRoute exact path="/login" component={LoginFormContainer} />
       <AuthRoute exact path="/signup" component={SignupFormContainer} />
+      <AuthRoute exact path="/scoreboard" component={ScoreboardContainer} />
     </Switch>
   </div>
 );
