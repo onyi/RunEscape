cc9420020827a315123efe09e6bb5dce60f34788
Onyi Cho <onyicho@gmail.com>
2019-07-17 15:49:15 -0700

    Refactor game file

--- frontend/src/components/game/game.jsx
+++ frontend/src/components/game/game.jsx
@@ -5,12 +5,13 @@ import openSocket from 'socket.io-client';
 import Player from './Player';
 import Skeleton from './Skeleton';
 
-import getready from '../../assets/game/get-ready.png';
-import gameover from '../../assets/game/game-over.png';
-import backgroundimg from '../../assets/game/background.png';
-import foregroundimg from '../../assets/game/foreground.png';
+import Background from './Background';
+import Foreground from './Foreground';
+
+import GameOver from './GameOver';
+import GetReady from './GetReady';
+
 import suddenatksound from '../../assets/game/lunatic_eyes.mp3';
-import gg from '../../assets/game/gg.mp3';
 import pointSound from '../../assets/game/sfx_point.wav';
 
 import Score from './Score';
@@ -19,16 +20,65 @@ class Game extends React.Component {
 
   constructor(props){
     super(props);
-    this.state = {
-      scores: props.scores
+    let cvs = document.getElementById('run-escape');
+    let ctx = cvs.getContext('2d');
+    this.cvs = cvs;
+    this.ctx = ctx;
+    this.gameState = {
+      current: 0,
+      getReady: 0,
+      game: 1,
+      over: 2,
     }
+    let rng = Prando(this.props.lobbyId);
+    this.rng = rng;
+    this.gameScore = new Score(cvs, ctx);
+    this.state = {
+      scores: props.scores,
+      gameState: 0,
+      localPlayerId: props.currentUser.id,
+      current: 0,
+      entities: [],
+      isOver: false,
+      frame: 0
+    };
     this.renderGame = this.renderGame.bind(this);
     this.socket = openSocket(window.location.origin);
+    this.lobbyId = props.lobbyId
+    this.mountController = this.mountController.bind(this);
+    this.frame = 0;
+    this.gameOver = new GameOver(this.cvs, this.ctx);
+    this.getReady = new GetReady(this.cvs, this.ctx);
+  }
 
+  gameOver(){
+    this.socket.emit("chat message", {
+      lobbyId: this.props.lobbyId,
+      msg: `${this.props.currentUser.username} met their end`
+    })
+    // gameOverAction();
+
+    this.socket.emit("relay game state", {
+      lobbyId: this.props.lobbyId,
+      gameState: this.gameState.over
+    });
+    this.props.postScore(this.gameScore.score);
+
+    this.setState({
+      current: this.gameState.over
+    })
   }
 
   componentDidMount() {
     this.props.getScores();
+    // this.setState({
+    //   cvs: document.getElementById('run-escape'),
+    //   ctx: cvs.getContext('2d')
+    // });
+    this.bg = new Background(this.cvs, this.ctx);
+    this.fg = new Foreground(this.cvs, this.ctx);
+    this.mountController();
+
     this.renderGame();
   }
 
@@ -36,12 +86,12 @@ class Game extends React.Component {
     this.socket.off(`relay action to ${this.props.match.params.lobbyId}`);
   }
 
-  mountController(state) {
+  mountController() {
     
-    this.socket.on(`relay action to ${this.props.match.params.lobbyId}`, 
+    this.socket.on(`relay action to ${this.props.lobbyId}`, 
       ({ playerId, playerAction}) => {
         console.log("Got input");
-        let players = state.entities.filter(entity => typeof Player)
+        let players = this.state.entities.filter(entity => typeof Player)
         let playerArr = players.filter(player => playerId === player.playerId);
         let player = playerArr[0];
         switch(playerAction) {
@@ -51,66 +101,97 @@ class Game extends React.Component {
           default:
             break;
         }
-      })
+      });
+
+    this.socket.on(`relay game state to ${this.props.lobbyId}`,
+      ({ lobbyId, gameState }) => {
+        console.log(`receive new game state from lobby ${lobbyId}`);
+        switch(gameState){
+          case 2:
+            console.log(`Game over son`);
+            this.setState({
+              gameState: 2
+            });
+          case 1:
+            console.log(`playing`);
+            this.setState({
+              gameState: 2
+            });
+          default:
+            break;
+        }
+      });
+  } 
+
+
+  draw() {
+    this.ctx.fillStyle = '#866286';
+    this.ctx.fillRect(0, 0, this.cvs.width, this.cvs.height);
+    this.bg.draw();
+    this.fg.draw();
+    this.state.entities.forEach(entity => entity.draw())
+    this.gameScore.draw(this.state);
+    this.getReady.draw(this.state);
+    this.gameOver.draw(this.state);
+  }
+
+  update() {
+    this.removeSkeleton();
+    this.state.entities.forEach(entity => entity.update(this.state))
+    this.state.gameScore.update(this.state);
+    this.bg.update();
+    this.fg.update();
+    this.generateSkeletons();
+  }
+
+  generateSkeletons() {
+    if (this.state.frames % (50 + (Math.floor(this.rng.next() * 25))) === 0 && this.state.current === this.gameState.game) {
+      this.state.entities.push(new Skeleton(this.cvs, this.ctx));
+    }
+  }
+
+  removeSkeleton() {
+    for (let i = 0; i < this.state.entities.length; i++) {
+      if (this.state.entities[i] instanceof Skeleton) {
+        if (this.state.entities[i].x < 0 - this.state.entities[i].w) {
+          delete this.state.entities[i];
+          i--;
+        }
+      }
+    }
+  }
+
+  removeSkeletons() {
+    for (let i = 0; i < this.state.entities.length; i++) {
+      if (this.state.entities[i] instanceof Skeleton) {
+        delete this.state.entities[i];
+        i--;
+      }
+    }
   }
   
   render() {
+
+
+    
     return (
       <canvas id="run-escape" width="800" height="500"></canvas>
     );
   }
 
   renderGame() {
+
     const that = this;
+    const lobby = this.props.lobbies[this.props.lobbyId];
 
-    //select cvs 
-    const cvs = document.getElementById('run-escape');
-    const ctx = cvs.getContext('2d');
-    const lobby = this.props.lobbies[this.props.match.params.lobbyId]
-    const rng = new Prando(lobby._id);
-
-    //game vars and consts
-    let frames = 0;
-
-    const state = {
-      localPlayerId: this.props.currentUser.id,
-      lobbyId: lobby._id,
-      current: 0,
-      getReady: 0,
-      game: 1,
-      over: 2,
-      entities: [],
-      gameScore: new Score(cvs, ctx),
-      gameOver: () => {
-        this.socket.emit("chat message", {
-          lobbyId: state.lobbyId,
-          msg: `${this.props.currentUser.username} met their end`
-        })
-        state.current = state.over;
-      }
-    }
     lobby.players.map(playerId => 
-      state.entities.push(new Player(cvs, ctx, playerId)))
-    state.entities.push(new Skeleton(cvs, ctx));
+      this.state.entities.push(new Player(this.cvs, this.ctx, playerId)))
+    this.state.entities.push(new Skeleton(this.cvs, this.ctx));
 
     //load sprite image
-    const ready = new Image();
-    ready.src = getready;
-
-    const over = new Image();
-    over.src = gameover;
-
-    const background = new Image();
-    background.src = backgroundimg;
-
-    const foreground = new Image();
-    foreground.src = foregroundimg;
 
     const gameplay_music = new Audio();
     gameplay_music.src = suddenatksound; 
-
-    const gameover_music = new Audio();
-    gameover_music.src = gg;
 
     const point_sound = new Audio();
     point_sound.src = pointSound;
@@ -118,191 +199,63 @@ class Game extends React.Component {
     //control the game state
     document.addEventListener('keydown', (e) => {
       if (e.keyCode === 32) {
-        switch (state.current) {
-          case state.getReady:
-            gameplay_music.currentTime = 0; 
-            gameover_music.currentTime = 0;
-            gameplay_music.play();
-            state.current = state.game;
+        switch (this.state.current) {
+          case this.gameState.getReady:
+            this.gameOver.gameplay_music.currentTime = 0; 
+            this.gameOver.gameover_music.currentTime = 0;
+            this.gameOver.gameplay_music.play();
+            this.state.current = this.gameState.game;
+            this.socket.emit("relay game state", {
+              lobbyId: this.lobbyId,
+              gameState: this.gameState.game
+            });
             break;
-          case state.game:
+          case this.gameState.game:
             // let players = state.entities.filter(entity => typeof Player)
             // let player = players.filter(player => state.localPlayerId === player.playerId);
             this.socket.emit("relay action", {
-              lobbyId: state.lobbyId,
-              playerId: state.localPlayerId,
+              lobbyId: this.lobbyId,
+              playerId: this.state.localPlayerId,
               playerAction: "hop"
             })
             break;
-          case state.over:
-            state.current = state.getReady;
-            removeSkeletons();
-            gameover_music.pause();   
-            gameOverAction();
+          case this.gameState.over:
+            this.setState({
+              current: this.gameState.getReady
+            })
+            this.removeSkeletons();
+            this.gameOver.gameover_music.pause();
+            this.socket.emit("relay game state", {
+              lobbyId: that.props.lobbyId,
+              gameState: this.gameState.getReady
+            });
             break;
         }
       }
     })
+  }
 
-    //background 
-    const bg = {
-      sX: 0,
-      sY: 0,
-      w: 800,
-      h: 500,
-      x: 0,
-      y: 0,
+  gameOverAction() {
+    this.socket.emit("relay game state", {
+      lobbyId: this.lobbyId,
+      gameState: this.gameState.over
+    });
+    this.props.postScore(this.gameScore.score);
+    // chara.reset();
+    this.bg.reset();
+    this.fg.reset();
+  }
 
-      dx: 10,
-
-      draw: function () {
-        ctx.drawImage(background, this.sX, this.sY, this.w, this.h, this.x, this.y, this.w, this.h)
-
-        ctx.drawImage(background, this.sX, this.sY, this.w, this.h, this.x + this.w, this.y, this.w, this.h)
-      },
-
-      update: function () {
-        if (this.dx = frames % 100 === 0 ? this.dx : this.dx)
-        if (state.current === state.game) {
-          this.x = (this.x - this.dx) % (this.w);
-        }
-      },
-
-      reset: function () {
-        this.dx = 10;
-      }
+  //loop
+  loop() {
+    this.update();
+    this.draw();
+    this.frames++;
+    // requestAnimationFrame(loop);
+    if (this.state.current === this.gameState.over) {
+      this.gameOver.gameplay_music.pause();
+      this.gameOver.gameover_music.play();
     }
-
-    //foreground
-    const fg = {
-      sX: 0,
-      sY: 0,
-      w: 800,
-      h: 59,
-      x: 0,
-      y: cvs.height - 59,
-
-      dx: 10,
-
-      draw: function () {
-        ctx.drawImage(foreground, this.sX, this.sY, this.w, this.h, this.x, this.y, this.w, this.h)
-
-        ctx.drawImage(foreground, this.sX, this.sY, this.w, this.h, this.x + this.w, this.y, this.w, this.h)
-      },
-
-      update: function () {
-        if (this.dx = frames % 100 === 0 ? this.dx : this.dx )
-        if (state.current === state.game) {
-          this.x = (this.x - this.dx) % (this.w);
-        }
-      },
-
-      reset: function() {
-        this.dx = 10;
-      }
-    }
-
-    //get ready message
-    const getReady = {
-      sX: 0,
-      sY: 0,
-      w: 141,
-      h: 50,
-      x: cvs.width / 2 - 141 / 2,
-      y: 150,
-
-      draw: function () {
-        if (state.current === state.getReady) {
-          ctx.drawImage(ready, this.sX, this.sY, this.w, this.h, this.x, this.y, this.w, this.h)
-          state.gameScore.reset();
-        }
-      }
-    }
-
-    const gameOver = {
-      sX: 0,
-      sY: 0,
-      w: 137,
-      h: 82,
-      x: cvs.width / 2 - 137 / 2,
-      y: 90,
-
-      draw: function () {
-        if (state.current === state.over) {
-          ctx.drawImage(over, this.sX, this.sY, this.w, this.h, this.x, this.y, this.w, this.h)
-        }
-      }
-    }
-
-    function generateSkeletons() {
-      if (frames % (50 + (Math.floor(rng.next() * 25))) === 0 && state.current === state.game) {
-        state.entities.push(new Skeleton(cvs, ctx));
-      }
-    }
-
-    function removeSkeleton(){
-      for (let i = 0; i < state.entities.length; i++) {
-        if (state.entities[i] instanceof Skeleton) {
-          if (state.entities[i].x <  0 - state.entities[i].w ){
-            delete state.entities[i];
-            i--;
-          }
-        }
-      }
-    }
-
-    function removeSkeletons() { 
-      for (let i = 0; i < state.entities.length; i++) {
-        if( state.entities[i] instanceof Skeleton ) {
-          delete state.entities[i];
-          i--;
-        }
-      }
-    }
-
-    function gameOverAction(){
-      that.props.postScore(state.gameScore.score);
-      // chara.reset();
-      bg.reset();
-      fg.reset();
-    }
-
-
-    //draw
-    function draw() {
-      ctx.fillStyle = '#866286';
-      ctx.fillRect(0, 0, cvs.width, cvs.height);
-      bg.draw();
-      fg.draw();
-      state.entities.forEach(entity => entity.draw())
-      state.gameScore.draw(state);
-      getReady.draw();
-      gameOver.draw();
-    }
-
-    function update() {
-      removeSkeleton();
-      state.entities.forEach(entity => entity.update(state))
-      state.gameScore.update(state);
-      bg.update();
-      fg.update();
-      generateSkeletons();
-    }
-
-    //loop
-    function loop() {
-      update();
-      draw();
-      frames++;
-      requestAnimationFrame(loop);
-      if (state.current === state.over) {
-        gameplay_music.pause();
-        gameover_music.play();
-      }
-    }
-
-    this.mountController(state);
-    loop();
   }
 }
 
