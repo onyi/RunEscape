738f6c7ef650684e66642a5d2b5e0869e79f9dbd
jiminey <jtwnguyen@ucdavis.edu>
2019-07-17 14:24:36 -0700

    Fix audio for dash and bg music

--- frontend/src/components/game/Player.js
+++ frontend/src/components/game/Player.js
@@ -2,7 +2,7 @@
 import reisen from '../../assets/game/reisen.png';
 import charajump from '../../assets/game/char.png'
 import jumpsound from '../../assets/game/jump_sound_effect.mp3';
-import airdashsound from '../../assets/game/sfx_swooshing.wav';
+import airdashsound from '../../assets/game/sfx_swooshing.mp3';
 
 class Player {
   constructor(canvas, context, playerId) {
